a29db81e3deb2dd43c061395f8c1fb73ae09fa60
왕발이 <114972710+wangbarri@users.noreply.github.com>
2023-02-08 19:53:30 +0900

    Update Test.py

--- TestCase/Test.py
+++ TestCase/Test.py
@@ -20,8 +20,9 @@ class TestRun(unittest.TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         cls.inter.driver.quit()
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test_result'))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRun)
     unittest.TextTestRunner(verbosity=2).run(suite)
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test_result'))
+
