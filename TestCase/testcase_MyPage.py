865208263355328b33bacaf6c307320502c9b946
왕발이 <114972710+wangbarri@users.noreply.github.com>
2023-02-08 14:13:39 +0900

    Update testcase_MyPage.py

--- TestCase/testcase_MyPage.py
+++ TestCase/testcase_MyPage.py
@@ -11,13 +11,13 @@ import time
 
 class TestCase(TestRun):
 
-    def test_1_Login(self):
-        login = Login(self.inter)
-        login.get()
-        login.Login_send_keys_ID()
-        login.Login_send_keys_PW()
-        login.Login_btn()
-        self.assertEqual(self.inter.driver.current_url, "https://www.musinsa.com/app/")
+   # def test_1_Login(self):
+     #   login = Login(self.inter)
+      #  login.get()
+      #  login.Login_send_keys_ID()
+      #  login.Login_send_keys_PW()
+       # login.Login_btn()
+       # self.assertEqual(self.inter.driver.current_url, "https://www.musinsa.com/app/")
 
     def test_2_Basket_List(self):
         BK = MypageList(self.inter)
@@ -33,14 +33,14 @@ class TestCase(TestRun):
         self.assertEqual(self.inter.driver.find_element(By.XPATH,"//*[@id='page_cart']/form[1]/div/div[1]/table/tbody/tr[1]/td/table/tbody/tr/td[3]/div[2]/p[2]").text, "[무신사 스탠다드] 스웨트셔츠 [블랙]")
         self.assertEqual(self.inter.driver.find_element(By.XPATH,"//*[@id='page_cart']/form[1]/div/div[1]/table/tbody/tr[3]/td/table/tbody/tr/td[3]/div[2]/p[2]").text, "[무신사 스탠다드] 베이식 긴팔 티셔츠 2팩")
         
-    def test_4_Check_mypage(self):
-        Cart = Check(self.inter)
-        Cart.Check_Mypage()
-        self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[0].text, "스웨트셔츠 [블랙]")
-        self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[1].text, "베이식 긴팔 티셔츠 2팩")
+    # def test_4_Check_mypage(self):
+      #  Cart = Check(self.inter)
+      #  Cart.Check_Mypage()
+      #  self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[0].text, "스웨트셔츠 [블랙]")
+      #  self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[1].text, "베이식 긴팔 티셔츠 2팩")
     
-    def test_5_Check_mypage(self):
-        Cart = Check(self.inter)    
-        Cart.Check_Like()
-        self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[0].text, "스웨트셔츠 [블랙]")
-        self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[1].text, "베이식 긴팔 티셔츠 2팩")
+    # def test_5_Check_mypage(self):
+       # Cart = Check(self.inter)    
+       # Cart.Check_Like()
+        # self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[0].text, "스웨트셔츠 [블랙]")
+        # self.assertEqual(self.inter.driver.find_elements(By.CLASS_NAME,"name")[1].text, "베이식 긴팔 티셔츠 2팩")
