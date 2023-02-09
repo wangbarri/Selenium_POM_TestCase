9223b97111ebc8223acfc935453a7fff7abeea8d
왕발이 <114972710+wangbarri@users.noreply.github.com>
2023-02-09 20:25:15 +0900

    Update testcase_natelogin.py

--- TestCase/testcase_natelogin.py
+++ TestCase/testcase_natelogin.py
@@ -30,6 +30,41 @@ class TestCases(TestRun):
         NI.ID_input_test()
         NI.next_page()
     
+    def testcase_4_PW(self):
+        NP = Nate_PW(self.inter)
+        NP.PW_page_check()
+        NP.PW_input_check()
+        NP.PW_input_test()
+        NP.input_PW_Check_test()
+        NP.Next_page()
 
+    def testcase_5_Email(self):
+        NE = Nate_Email(self.inter)
+        NE.Email_page_check()
+        NE.Email_input_test()
+        NE.Next_page()
+
+    def testcase_6_Other_information(self):
+        NO = Nate_Information(self.inter)
+        NO.Information_page_check()
+        NO.Information_Name()
+        NO.Information_date()
+        NO.Information_gender()
+        NO.under_14_Next_page()
+        NO.Up_14_Next_page()
+
+    def testcase_7_Privacy(self):
+        NP = Nate_Privacy(self.inter)
+        NP.Privacy_page_check()
+        NP.Privacy_Radio_btn()
+        NP.Next_page()
+
+    def testcase_8_End(self):
+        NE = Nate_End(self.inter)
+        NE.End_page_check()
+
+    def testcase_9__Login(self):
+        NL = Nate_Login(self.inter)
+        NL.Login_page_check()
 
 
